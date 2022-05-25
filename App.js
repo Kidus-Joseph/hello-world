a224062e17ed55abf0954dfa1e287993f89e915d
Kidus Joseph <josephkidus@gmail.com>
2022-05-24 21:29:26 -0700

    fix

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello IS657 from Kidus Joseph</Text>
+      <Text>Hello IS657 from Kidus Joseph!</Text>
       <StatusBar style="auto" />
     </View>
   );
